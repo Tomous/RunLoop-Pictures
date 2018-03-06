a40c83a7d3593f197425ec8b594fef46432849f8
wenhua <xudacheng_tomous@163.com>
2018-03-06 14:53:02 +0800

    Initial Commit

---
+++ 循环滚动图片/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  循环滚动图片
+//
+//  Created by wenhua on 2018/3/6.
+//  Copyright © 2018年 wenhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
